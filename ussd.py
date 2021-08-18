294469a74de3541093274a0e78920881e5ea834e
amRadhwen <radhweneamri9631@gmail.com>
2021-08-18 17:47:06 +0100

    last update

--- ussd.py
+++ ussd.py
@@ -8,19 +8,44 @@ class USSD:
 
     
     def execUSSD(self, ussd_string):
-        self.response = None
-        try:
-            self.response = self.modem.sendUssd(ussd_string, 5)
-            print(self.response.message)
-            #self.sessionControl()
-            return self
-        except CmeError:
-            print("Invalid USSD mode !")
-            return False
+        ussd_code_string = self.validateUssd(ussd_string)
+        if ussd_code_string:
+            self.response = None
+            try:
+                self.response = self.modem.sendUssd(ussd_code_string[0], 5)
+                #print(self.response.message)
+                for responseText in ussd_code_string[1:]:
+                    print(responseText)
+                    #self.sessionControl(responseText)
+                return self
+            except CmeError:
+                print("Invalid USSD mode !")
+                return False
+            else:
+                print("Unexpected Error, Try Again !")
+                return False
         else:
-            print("Unexpected Error, Try Again !")
             return False
     
     def sessionControl(self, repText):
         while self.response.sessionActive:
+            print(repText)
             self.response = self.response.reply(repText)
+            print(self.response.message)
+    
+    def validateUssd(self, ussdCodeText):
+        ussdSeq = []
+        if ussdCodeText.startswith("*") and ussdCodeText.endswith("#"):
+            index = ussdCodeText.find("*", 1)
+            if index != -1:
+                ussdSeq.append(ussdCodeText[0:index]+"#")
+                while index != -1:
+                    index = ussdCodeText.find("*",index+1)
+                    ussdSeq.append(ussdCodeText[index-1])
+                #ussdSeq.append("#")
+                return ussdSeq
+            else:
+                ussdSeq.append(ussdCodeText)
+                return ussdSeq
+        else:
+            return False
